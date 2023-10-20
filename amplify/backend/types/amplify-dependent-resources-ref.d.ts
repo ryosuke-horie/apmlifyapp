b19a85d12eec302c0c0ddbf73ed2101b50e830c4
fsworks\r-horie <r-horie@mamiya-its.co.jp>
2023-10-20 14:13:26 +0900

    create api chapter4

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "apmlifyreactgraphgl": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "apmlifyreactgraphglae53c7f5": {
       "AppClientID": "string",
